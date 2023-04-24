e0ebaa85ab76bd6660fa0eecc2575168243c81dd
Finn <finn@callpipe.com>
2023-04-24 13:34:28 -0700

    validate incoming SMSs from acceleratenetworks provider

--- hook/sms_hook_acceleratenetworks.php
+++ hook/sms_hook_acceleratenetworks.php
@@ -8,13 +8,20 @@ set_include_path(parse_ini_file($conf[0])['document.root']);
 require_once "resources/require.php";
 require_once "../sms_hook_common.php";
 
-// if ($debug) {
-// 	error_log('[SMS] REQUEST: ' .  print_r($_SERVER, true));
-// }
-
 $data = json_decode(file_get_contents("php://input"));
 if ($debug) {
 	error_log('[SMS] REQUEST: ' .  print_r($data, true));
 }
+
+$sql = "SELECT default_setting_value FROM v_default_settings WHERE default_setting_category = 'sms' AND default_setting_subcategory = 'acceleratenetworks_inbound_token'";
+$database = new database;
+$token = $database->select($sql, null, 'column');
+unset($parameters);
+
+if($data->ClientSecret != $token) {
+	error_log('ACCESS DENIED [SMS] invalid or missing ClientSecret token');
+	die("access denied");
+}
+
 
 route_and_send_sms($data->{'From'}, $data->{'To'}, $data->{'Content'});
